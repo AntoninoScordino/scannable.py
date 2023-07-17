90d212dcac0d5e911aa27f2b4d6e4691b7815c82
lixoh16 <135661010+AntoninoScordino@users.noreply.github.com>
2023-07-17 20:14:05 +0000

    scannable.py: Add default values for required variables
    * Without those, the URL can not be generated. Set some default values
      that can be obviously overrided later with the parameters we created
      in the previous commit.

--- scannable/utils/generate.py
+++ scannable/utils/generate.py
@@ -6,6 +6,14 @@
 
 class Generate:
     def __init__(self, format, background, text, size, link):
+
+        # Set default values. They are required, otherwise the picture
+        # won't be generated at all.
+        format = 'png'
+        background = '000000'
+        text = 'white'
+        size = '512'
+
         self.format = format
         self.background = background
         self.text = text
