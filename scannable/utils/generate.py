e775be1d824b4201a20bac706793e54852a02ffe
lixoh16 <135661010+AntoninoScordino@users.noreply.github.com>
2023-07-17 20:16:18 +0000

    scannable.py: Generate URI from link through `spotify-uri`
    * Also, remove the '$' that can sometimes pop-up while generating an URI.
      Appeared while trying to generate it through [1].
    
    [1] :- https://open.spotify.com/artist/3srPc1Mytv5GmTWqsQuoXW

--- scannable/utils/generate.py
+++ scannable/utils/generate.py
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: WTFPL
 #
 
+from spotify_uri import formatURI
+
 class Generate:
     def __init__(self, format, background, text, size, link):
 
@@ -19,4 +21,13 @@ class Generate:
         self.text = text
         self.size = size
         self.link = link
-        
+
+        # Gather the ur from the provided link __only__ if the link
+        # contains 'open.spotify.com'.
+        if 'open.spotify.com' in self.link:
+            link = formatURI(self.link)
+            # Remove the '$' from the generated 'formatURI'. For whatever
+            # reason, it gets generated with some URL, while with other it
+            # does not. For example, https://open.spotify.com/artist/3srPc1Mytv5GmTWqsQuoXW.
+            if '$' in link:
+                link = link.replace("$","")
