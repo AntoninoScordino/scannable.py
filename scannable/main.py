f2afade5b7046d9f0ecb408496b501c0196056b5
lixoh16 <135661010+AntoninoScordino@users.noreply.github.com>
2023-07-18 03:14:59 +0000

    scannable.py: Change way to set default values
    * Doing so straight from the 'Generate' class was just a mess, and was
      overriding every input the user would choose. For example, if we gave
      to the parser the '-b ff00000' argument, the class would totally ignored
      it and rather pick the default '000000'.
    
    * To prevent this from happening, set default values through 'argparse'
      itself. Did not know about this functionality, have to be honest. But
      if it works, and does its job __properly__, it's fine.

--- scannable/main.py
+++ scannable/main.py
@@ -9,11 +9,11 @@ from utils.generate import Generate
 
 parser = argparse.ArgumentParser(prog='scannable.py',
                                  description='Generate Spotify scannable code through a Python script.')
-parser.add_argument('link', help='Spotify link to generate a scannable image from')
-parser.add_argument('-f', '--format', help='file format for the scannable output')
-parser.add_argument('-b', '--background', help='background color for the scannable output')
-parser.add_argument('-t', '--text', help='text color for the scannable output')
-parser.add_argument('-s', '--size', help='size of the scannable output')
+parser.add_argument('link', help='Spotify link to generate a scannable image from', default=None)
+parser.add_argument('-f', '--format', help='file format for the scannable output', default='png')
+parser.add_argument('-b', '--background', help='background color for the scannable output', default='000000')
+parser.add_argument('-t', '--text', help='text color for the scannable output', default='white')
+parser.add_argument('-s', '--size', help='size of the scannable output', default='512')
 
 args = parser.parse_args()
 
